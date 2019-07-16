794144e6f918d9623b260a263e8f20a1b3634c9e
JiangJf <1594426983@qq.com>
2019-07-16 14:32:50 +0800

    改善了knowledge包中的TextEquals类（关于equals()方法的知识）

--- src/com/knowledge/TextEquals.java
+++ src/com/knowledge/TextEquals.java
@@ -1,21 +1,27 @@
 package com.knowledge;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
- * 测试Object类的实现原理
+ * 测试Object类的 euqals()方法的实现原理
  * @aothor jjf
  * @version 1.0.1  2017-07-16
  *
  * 结论：
  *      Object类中的equals方法默认比较的的是两个对象变量的引用值是否相等，即是否为同一变量；
+ *
  * 所以：在使用equals方法时，如果调用的对象是自定义的类，而又没有重写equals方法，则比较的是引用值
  *       在使用由java类库中的类所产生的实例调用equals方法时，一定要查明该实例是否重写了equals方法；
+ *
  * 注意1：String类中重写了equals方法，使其功能为：如果两个String实例变量的引用的内容相等，则结果为true
- *       数组类型数据虽然作为对象处理，但是没有为其重写Object类的equals方法。
+ *       数组类型数据虽然作为对象处理，但是没有为其重写Object类的equals方法，所以比较的是引用而不是数组
+ *       的值，但可以是用Arrays 类的静态方法 Arrays.equals(Object[], Object[])来比较两个数组的元素是否相等。
  *       对于基本数据类型，必须先将其转化为类类型才可以调用equals方法，结果比较的是值是否相等。
+ *
  * 注意2：如果是针对自定义的类，想要有自己的equals()方法逻辑，除了需要考虑需要实现值相等 or 引用相等外
  *        还有保证按照java纠纷的要求保留equals()方法的5个特性：
+ *
  *          1.自反性：x.equals(x) 结果为true，这一般都会成立
  *          2.对称性：若x.equals(y)结果为true ，则 y.equals()结果也必须为true。这在于有继承关系的类模块中容易出错
  *                    即有继承关系的类模块，需要注意考虑子类之间是否equals()逻辑不同。如果是这往往要考虑对称性，
@@ -40,26 +46,19 @@ public class TextEquals {
         String string1 = new String("jjf");
         String string2 = new String("jjf");
         int[] arrint1 = {1, 2, 3};
-        int[] arrint2 = {1, 2, 2};
+        int[] arrint2 = {1, 2, 3};
         Integer a = new Integer(3);
         int b = 3;
 
 
-        System.out.println("self class equals: " + textEquals1.equals(textEquals2));
-        System.out.println("String equals " + string1.equals(string2));
-        System.out.println("arrays equals " + arrint1.equals(arrint2));
-        System.out.println("int equals " + ((Integer)a).equals(3));
+        System.out.println("self class equals: " + textEquals1.equals(textEquals2)); //false
+        System.out.println("String equals " + string1.equals(string2)); //true
+        System.out.println("arrays equals " + arrint1.equals(arrint2)); //false
+        System.out.println("use Arrays.equals() 测试数组的相等性 " + Arrays.equals(arrint1, arrint2)); //true
+        System.out.println("int equals " + ((Integer)a).equals(3)); //true
 
-        System.out.println(Objects.equals(string1, string2));
-        System.out.println(Objects.equals(textEquals1, textEquals2));
-        /**
-         * 结论：Object类中的equals方法默认比较的的是两个对象变量的引用值是否相等，即是否为同一变量；
-         * 所以：在使用equals方法时，如果调用的对象是自定义的类，而又没有重写equals方法，则比较的是引用值
-         *       在使用由java类库中的类所产生的实例调用equals方法时，一定要查明该实例是否重写了equals方法；
-         * 注意：String类中重写了equals方法，使其功能为：如果两个String实例变量的引用的内容相等，则结果为true
-         *       数组类型数据虽然作为对象处理，但是没有为其重写Object类的equals方法。
-         *       对于基本数据类型，必须先将其转化为类类型才可以调用equals方法，结果比较的是值是否相等。
-         */
+        System.out.println(Objects.equals(string1, string2)); //true
+        System.out.println(Objects.equals(textEquals1, textEquals2)); //false
 
     }
 
